c957dc422ed46176f0658d1dd06d417f82440ccd
MJKM <rhkrrbaudgg@naver.com>
2023-02-03 22:05:52 +0900

    ✨ add: 플레이어 상태 관리

---
+++ Manager/PlayerStatus.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+    public static bool isAttack = false;
+    public static bool isJump = false;
+    public static bool isAir = false;
+    public static bool isSkill = false;
+}
