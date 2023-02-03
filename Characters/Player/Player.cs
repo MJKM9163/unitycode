0a7b5c64188e8b83350a6ccef7e2f332e00f40f0
MJKM <rhkrrbaudgg@naver.com>
2023-02-03 22:09:08 +0900

    :construction: update: 스킬 움직임 구현, 점프 수정

--- Characters/Player/Player.cs
+++ Characters/Player/Player.cs
@@ -6,9 +6,7 @@ public class Player : MonoBehaviour
 {
     public GameManager gm;
     public GameOption option;
-
     public float maxSpeed;
-    bool isAir;
     public float jumpPower;
     float jumpZ;
     CapsuleCollider2D coll;
@@ -25,13 +23,15 @@ public class Player : MonoBehaviour
     SpriteRenderer spriter;
     Animator anim;
 
+
+    WaitForSeconds delay003 = new WaitForSeconds(0.03f);
+
     // 초기 설정
     void Awake() {
         rigid = GetComponent<Rigidbody2D>();
         spriter = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
         maxSpeed = 40;
-        isAir = false;
         jumpPower = 4;
         jumpZ = 0;
         coll = GetComponent<CapsuleCollider2D>();
@@ -44,8 +44,12 @@ public class Player : MonoBehaviour
 
     void move() {
         float x = Mathf.Clamp(rigid.position.x, -109f, 109f);
-        float y = Mathf.Clamp(rigid.position.y, -22f, 48f + (isAir ? 20f : 0f));
-        rigid.MovePosition(new Vector2(x, y) + nextVec);
+        float y = Mathf.Clamp(rigid.position.y, -35f, 35f + (PlayerStatus.isAir ? 20f : 0f));
+        if (PlayerStatus.isJump) {
+            rigid.velocity = new Vector2(nextVec.x * maxSpeed, rigid.velocity.y);
+        } else {
+            rigid.velocity = nextVec * maxSpeed;
+        }
 
         if (nextVec.x != 0 || nextVec.y != 0) {
             anim.SetInteger("move", 1);
@@ -70,7 +74,7 @@ public class Player : MonoBehaviour
         atacando = true;
         attackCombo = 0;
         anim.SetInteger("combo", attackCombo);
-        //gm.Action("");
+        PlayerStatus.isAttack = false;
     }
 
     // void Start()
@@ -82,7 +86,7 @@ public class Player : MonoBehaviour
         Debug.Log("점프!");
         float YSave = rigid.position.y;
         bool isJump = true; // 현재 점프 중
-        isAir = true; // 현재 공중에 떠있는 중
+        PlayerStatus.isAir = true; // 현재 공중에 떠있는 중
         anim.SetBool("jump", true);
         vLock = true;
         
@@ -104,7 +108,7 @@ public class Player : MonoBehaviour
                     anim.SetBool("down", false);
                     anim.SetInteger("move", 0);
                     vLock = false;
-                    isAir = false;
+                    PlayerStatus.isAir = false;
                     break;
                 }
             }
@@ -114,6 +118,53 @@ public class Player : MonoBehaviour
         }
     }
 
+    IEnumerator Skill1() {
+        PlayerStatus.isSkill = true; // 임시 스킬 사용중 표시(방향, 움직임 등 제한 기능)
+        float speed = spriter.flipX ? 1f : -1f;
+        rigid.AddForce(transform.right * (spriter.flipX ? -91f : 91f), ForceMode2D.Impulse);
+        while(spriter.flipX ? speed <= 13f : speed >= -13) {
+            rigid.AddForce(transform.right * speed, ForceMode2D.Impulse);
+            if (spriter.flipX) {
+                speed += 1;
+            } else {
+                speed -= 1;
+            }
+            yield return delay003;
+
+        }
+        PlayerStatus.isSkill = false;
+    }
+
+    IEnumerator JumpTest() {
+        PlayerStatus.isJump = true;
+        PlayerStatus.isAir = true;
+        Vector3 minus = rigid.position;
+        float YSave = rigid.position.y;
+        float speed = -1f;
+        vLock = true;
+        rigid.AddForce(transform.up * 55f, ForceMode2D.Impulse);
+        anim.SetBool("jump", true);
+        while(speed >= -20) {
+            if (speed == -10) {
+                anim.SetBool("jump", false);
+                anim.SetBool("down", true);
+            }
+            rigid.AddForce(transform.up * speed, ForceMode2D.Impulse);
+            speed -= 1;
+            if (rigid.position.y < YSave) {
+                break;
+            }
+            yield return delay003;
+        }
+        rigid.velocity = Vector3.zero;
+        minus.x = rigid.position.x;
+        rigid.position = minus;
+        vLock = false;
+        anim.SetBool("down", false);
+        anim.SetInteger("move", 0);
+        PlayerStatus.isJump = false;
+        PlayerStatus.isAir = false;
+    }
 
     // 컴퓨터 성능에 따라 프레임이 다르다.
     // 단순한 로직 업데이트에 사용
@@ -122,14 +173,18 @@ public class Player : MonoBehaviour
 
         // 락이 걸려있지 않으면 움직임 입력을 받음
         inputVec.x = hLock ? 0 : Input.GetAxisRaw("Horizontal");
-        inputVec.y = vLock ? 0 : Input.GetAxisRaw("Vertical");
+        inputVec.y = (vLock || PlayerStatus.isJump) ? 0 : Input.GetAxisRaw("Vertical");
 
-        // 좌 or 우 입력 버튼에서 땠을 때 확인
+        // 움직임 입력 버튼에서 손 땠음을 때 확인
+        // 점프 중이 아닐 때 확인
         // 속도를 0으로 설정
-        // 런 애니메이션 비활성화
-        if (Input.GetButtonUp("Horizontal") && inputVec.y == 0) {
-            anim.SetInteger("move", 0);
-        } else if (Input.GetButtonUp("Vertical") && inputVec.x == 0) {
+        if (Input.GetButtonUp("Horizontal") || Input.GetButtonUp("Vertical") &&
+            !PlayerStatus.isJump) {
+            rigid.velocity = Vector2.zero;
+        }
+
+        // 캐릭터가 멈춰있다면 런 애니메이션 비활성화
+        if (inputVec.y == 0 && inputVec.x == 0) {
             anim.SetInteger("move", 0);
         }
 
@@ -137,6 +192,7 @@ public class Player : MonoBehaviour
             !anim.GetCurrentAnimatorStateInfo(0).IsName("jump") &&
             !anim.GetCurrentAnimatorStateInfo(0).IsName("down")
         ) {
+            PlayerStatus.isAttack = true;
             attackAnimCheck = true;
             atacando = false;
             anim.SetTrigger("attack");
@@ -153,6 +209,24 @@ public class Player : MonoBehaviour
             anim.SetInteger("move", 0);
         }
 
+        if (Input.GetMouseButtonDown(1)) {
+            Debug.Log("마우스 우클릭");
+            StartCoroutine("Skill1");
+        }
+
+        // 현재 실행중인 애니메이션이 점프가 아닐 경우
+        // 점프(스페이스)를 눌렀을 경우, 캐릭터 y축 속도가 0일 경우
+        // 캐릭터의 +y축으로 힘을 가한다.
+        // 점프 애니메이션 실행을 위해 jump를 활성화한다.
+        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("jump") &&
+            !anim.GetCurrentAnimatorStateInfo(0).IsName("down") &&
+            Input.GetButtonDown("Jump")) {
+            Debug.Log("스페이스 클릭!");
+            rigid.velocity = Vector2.zero;
+            //StartCoroutine("JumpCo");
+            StartCoroutine("JumpTest");
+        }
+
     }
 
     void OnDrawGizmos() {
@@ -163,26 +237,18 @@ public class Player : MonoBehaviour
     // 고정된 프레임으로 업데이트!
     // 물리 연산 로직에 주로 사용
     void FixedUpdate() {
-
         nextVec = inputVec.normalized * maxSpeed * Time.fixedDeltaTime;
 
         // 현재 공격중이 아닐 때
         // 앞, 뒤로 움직일 수 있다.
         if (!attackAnimCheck) {
-            if (inputVec.x != 0 || inputVec.y != 0) {
+            if ((inputVec.x != 0 || inputVec.y != 0)
+                // !anim.GetCurrentAnimatorStateInfo(0).IsName("jump") &&
+                // !anim.GetCurrentAnimatorStateInfo(0).IsName("down")) {
+                    ) {
                 option.indexAdjustment(spriter, rigid);
                 move();
                 Debug.Log("움직이는 중");
-            }
-
-            // 현재 실행중인 애니메이션이 점프가 아닐 경우
-            // 점프(스페이스)를 눌렀을 경우, 캐릭터 y축 속도가 0일 경우
-            // 캐릭터의 +y축으로 힘을 가한다.
-            // 점프 애니메이션 실행을 위해 jump를 활성화한다.
-            if (!anim.GetCurrentAnimatorStateInfo(0).IsName("jump") &&
-                !anim.GetCurrentAnimatorStateInfo(0).IsName("down") &&
-                Input.GetButton("Jump")) {
-                StartCoroutine("JumpCo");
             }
         }
     }
@@ -195,14 +261,14 @@ public class Player : MonoBehaviour
         // 캐릭터 방향에 따라 히트박스 위치 옮기기
         if (inputVec.x < 0 && !attackAnimCheck && !spriter.flipX) {
             spriter.flipX = true;
-            Vector3 minusOne = attackHitPos.position;
-            minusOne.x -= 1.74f;
-            attackHitPos.position = minusOne;
+            Vector3 minus = attackHitPos.position;
+            minus.x -= 17.4f;
+            attackHitPos.position = minus;
 
         } else if (inputVec.x > 0 && !attackAnimCheck && spriter.flipX) {
             spriter.flipX = false;
             Vector3 minusOne = attackHitPos.position;
-            minusOne.x += 1.74f;
+            minusOne.x += 17.4f;
             attackHitPos.position = minusOne;
         }
     }
